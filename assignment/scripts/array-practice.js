e930f2cfffdec29677bcaf15628122e28431529f
Christopher Black <chris@primeacademy.io>
2023-06-30 15:49:35 -0500

    Added unit tests.

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -1,55 +1,67 @@
 console.log('****** Array Practice *******');
 
-// 1. Creating arrays
+// Creating arrays
 console.log('--- 1. Practice creating arrays ---');
 
 // Example: Array of animals
 let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
-// 1.a. TODO: Make an array with some favorite foods
+// 1. TODO: Make an array `favoriteFoods` with some favorite foods
+//    Console log your array of foods to the console with a message, 
+//    similar to the example above
 
 
-// 1.b. TODO: Log your array of foods to the console with a message, similar 
-//      to the example above
-
-
-
-// 2. Array.length - tells you how many items are in the array
+// Array.length - tells you how many items are in the array
 console.log('--- 2. Length of an array ---');
 
-// Example: How many animals are in the array? 
-console.log('Number of animals:', animalArray.length);
+// Example: How many animals are in the array?
+let numberOfAnimals = animalArray.length
+console.log('Number of animals:', numberOfAnimals);
 
-// 2.a. TODO: Log to the console the number of foods in your array
+// 2. TODO: Create a variable `numberOfFoods`
+//    Console log to the console the number of foods
 
 
-// 3. Accessing array items
+// Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
 // Example: Log the first animal from the array using it's array index
-console.log('First animal is', animalArray[0])
+let firstAnimal = animalArray[0];
+console.log('First animal is', firstAnimal);
 
-// 3.a. TODO: Log the second animal in the array 
-
-
-// 3.b. TODO: Log the last animal in the array using it's array index 
-
-
-// 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
-//      instead of the exact index number of the last item
+// 3.a. TODO: Create a variable `secondAnimal`
+//      Console log the second animal in the array 
 
 
+// 3.b. TODO: Create a variable `lastAnimal`
+//      Console log the last animal in the array using it's array index 
 
-// 4. Adding & Removing Array Items
+
+// 3.c. (STRETCH) TODO: Update 3.b to use the array length, 
+//      instead of the exact index number of the last item.
+
+
+
+// Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
 
 // Example: Add an animal to the end using Array.push
-animalArray.push('penguin');
+let animalToPush = 'penguin';
+animalArray.push(animalToPush);
 console.log('Added an animal to end,', animalArray);
 
-// 4.a. TODO: Add a new food at the end of your array & log the array
+// 4.a. TODO: Create a variable `dessert` and assign it to a new food.
+//      Add the `dessert` at the end of your array & log the array
 
+
+// Example: Add an animal to the beginning using Array.unshift
+let animalToUnshift = 'walrus';
+animalArray.unshift(animalToUnshift);
+console.log(`Added an animal to beginning: ${animalArray}`);
+
+// 4.b. TODO: Create a variable `entree` and assign it to a new food.
+//      Add the `entree` at the beginning of the array & log the array
 
 
 // Example: Remove the last animal by using Array.pop
@@ -57,38 +69,70 @@ let removedAnimal = animalArray.pop();
 console.log('Removed the last animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
-// 4.b. TODO: Remove the food at the end of your array & 
-//      log both the food removed and the updated array
+// NOTE: Use the `colors` array for 4.c and 4.d.
+let colors = ['orange', 'yellow', 'green', 'teal'];
+
+// 4.c. TODO: Create a variable `lastColor`. Remove the color 
+//      at the end of your array & assign it to `lastColor`.
+//      Console log both the color removed and the updated array.
 
 
-// Example: Add an animal to the beginning using Array.unshift
-animalArray.unshift('walrus');
-console.log(`Added an animal to beginning: ${animalArray}`);
-
-// 4.c. TODO: Add a food at the beginning of the array & log the array
-
-
-// Example: Remove the first animal using Array.shift
+// Example: Remove the first color using Array.shift
 removedAnimal = animalArray.shift();
 console.log('Removed the first animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
-// 4.d TODO: Remove the food at the beginning of your array & 
-//     log both the food removed and the updated array
+// 4.d. TODO: Create a variable `firstColor`. Remove the color 
+//      at the beginning of your array & assign it to `firstColor`.
+//      Console log both the color removed and the updated array.
 
-// 4.e (STRETCH) TODO: Replace the second food in your array
-//      with another one of your favorite foods.
-//      Then log the updated array.
 
-// 4.f (STRETCH) TODO: Sort your favoriteFoods array
-//     in reverse alphabetical order. Log the array.
+// STRETCH GOALS:
 
-// 4.g (STRETCH) TODO: Convert your array to a string
-//     putting the word "and" between each item.
-//     eg "tacos and pizza and pasta". Log the string.
+let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 
-// 4.h (STRETCH) TODO: Make a new array that combines 
-//     the favorite foods array with the animals array.
-//     Then log the new array.
+// 5.a (STRETCH) TODO: Replace the second gem with 'Tourmaline'
+//      Console log the updated array.
+
+
+// 5.b (STRETCH) TODO: Sort your gems array
+//     in reverse alphabetical order.
+//     Console log the array.
+
+
+// 5.c (STRETCH) TODO: Create a variable `gemString` and
+//     put the word "and" between each item. Research `.join()`.
+//     eg "Tourmaline and Topaz and Tanzanite and Iolite and Emerald and Diamond".
+//     Console log the string.
+
+
+// 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines 
+//     the gems array with the colors array.
+//     Console log the new array.
 //     It should look something like:
-//     ['pizza', 'pasta', 'fish', 'cat', 'bird', 'dog']
+//     ['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']
+
+
+
+
+
+// DO NOT MODIFY
+// Used for automated testing
+try {
+  module.exports = {
+    favoriteFoods: typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
+    numberOfFoods: typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
+    secondAnimal: typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
+    lastAnimal: typeof lastAnimal !== 'undefined' ? lastAnimal : undefined,
+    dessert: typeof dessert !== 'undefined' ? dessert : undefined,
+    entree: typeof entree !== 'undefined' ? entree : undefined,
+    lastColor: typeof lastColor !== 'undefined' ? lastColor : undefined,
+    firstColor: typeof firstColor !== 'undefined' ? firstColor : undefined,
+    gems: typeof gems !== 'undefined' ? gems : undefined,
+    gemString: typeof gemString !== 'undefined' ? gemString : undefined,
+    colorfulGems: typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
+    colors: typeof colors !== 'undefined' ? colors : undefined,
+  };
+} catch (e) {
+  // Do nothing
+}
---
+++ test/test.js
@@ -0,0 +1,142 @@
+/**
+ * We have to do a bit of work upfront to allow the tests
+ * to run in the browser and in Node.js. 
+ */
+let assert, expect;
+let testItems = {};
+if (typeof window === 'object') {
+    // Run tests in browser
+    assert = chai.assert;
+    expect = chai.expect;
+    mocha.setup('bdd');
+    testItems = {
+        // Variables
+        favoriteFoods: typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
+        numberOfFoods: typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
+        secondAnimal: typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
+        lastAnimal: typeof lastAnimal !== 'undefined' ? lastAnimal : undefined,
+        dessert: typeof dessert !== 'undefined' ? dessert : undefined,
+        entree: typeof entree !== 'undefined' ? entree : undefined,
+        lastColor: typeof lastColor !== 'undefined' ? lastColor : undefined,
+        firstColor: typeof firstColor !== 'undefined' ? firstColor : undefined,
+        gems: typeof gems !== 'undefined' ? gems : undefined,
+        gemString: typeof gemString !== 'undefined' ? gemString : undefined,
+        colorfulGems: typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
+        colors: typeof colors !== 'undefined' ? colors : undefined,
+    };
+} else {
+    // Run tests in Node.js
+    assert = require('assert');
+    expect = require('chai').expect;
+    testItems = require('../assignment/scripts/array-practice.js');
+}
+
+/**
+ * Put all tests within this describe.
+ */
+describe('Automated tests', function () {
+    describe(`Favorite Foods Array created`, function () {
+        it(`Favorite Foods Array created`, function () {
+            let { favoriteFoods } = testItems;
+            expect(favoriteFoods, 'Expected favoriteFoods to be exist').to.exist;
+            expect(favoriteFoods).to.be.a('array');
+            expect(favoriteFoods.length).to.be.greaterThan(0);
+        });
+    });
+    describe(`Used .length property to find array length`, function () {
+        it(`Used .length property to find array length`, function () {
+            let { numberOfFoods } = testItems;
+            expect(numberOfFoods, 'Expected lastName to be a number').to.exist;
+            expect(numberOfFoods).to.be.a('number');
+            expect(numberOfFoods).to.be.greaterThan(0);
+        });
+    });
+    describe(`Second animal is saved in a variable`, function () {
+        it(`Second animal is saved in a variable`, function () {
+            let { secondAnimal } = testItems;
+            expect(secondAnimal, 'Expected secondAnimal to be a string').to.exist;
+            expect(secondAnimal).to.be.a('string');
+            expect(secondAnimal).to.equal('cat');
+        });
+    });
+    describe(`Last animal is saved in a variable`, function () {
+        it(`Last animal is saved in a variable`, function () {
+            let { lastAnimal } = testItems;
+            expect(lastAnimal, 'Expected lastAnimal to be a string').to.exist;
+            expect(lastAnimal).to.be.a('string');
+            expect(lastAnimal).to.equal('dog');
+        });
+    });
+    describe(`Used .push() to add item to end of array`, function () {
+        it(`Used .push() to add item to end of array`, function () {
+            let { dessert, favoriteFoods } = testItems;
+            expect(dessert, 'Expected dessert to be a string').to.exist;
+            expect(dessert).to.be.a('string');
+            expect(dessert).to.equal(favoriteFoods[favoriteFoods.length - 1]);
+        });
+    });
+    describe(`Used .unshift() to add item to beginning of array`, function () {
+        it(`Used .unshift() to add item to beginning of array`, function () {
+            let { entree, favoriteFoods } = testItems;
+            expect(entree, 'Expected dessert to be a string').to.exist;
+            expect(entree).to.be.a('string');
+            expect(entree).to.equal(favoriteFoods[0]);
+        });
+    });
+    describe(`Used .pop() to remove the item from end of array`, function () {
+        it(`Used .pop() to remove the item from end of array`, function () {
+            let { lastColor, colors } = testItems;
+            expect(lastColor, 'Expected lastColor to be a string').to.exist;
+            expect(lastColor).to.be.a('string');
+            expect(lastColor).to.equal('teal');
+            expect(colors).to.be.an('array').that.does.not.include('teal');
+        });
+    });
+    describe(`Used .shift() to remove the item from beginning of array`, function () {
+        it(`Used .shift() to remove the item from beginning of array`, function () {
+            let { firstColor, colors } = testItems;
+            expect(firstColor, 'Expected firstColor to be a string').to.exist;
+            expect(firstColor).to.be.a('string');
+            expect(firstColor).to.equal('orange');
+            expect(colors).to.be.an('array').that.does.not.include('orange');
+        });
+    });
+    describe(`STRETCH: Replace 'Citrine' with 'Tourmaline'`, function () {
+        it(`STRETCH: Replace 'Citrine' with 'Tourmaline'`, function () {
+            let { gems } = testItems;
+            expect(gems).to.be.an('array').that.does.not.include('Citrine');
+            expect(gems).to.be.an('array').that.does.include('Tourmaline');
+        });
+    });
+    describe(`STRETCH: Sort the gems in reverse alphabetical order`, function () {
+        it(`STRETCH: Sort the gems in reverse alphabetical order`, function () {
+            let { gems } = testItems;
+            expect(gems).to.eql(['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond']);
+        });
+    });
+    describe(`STRETCH: Gem string with all gems`, function () {
+        it(`STRETCH: Gem string with all gems`, function () {
+            let { gemString } = testItems;
+            if (gemString === undefined) {
+                this.skip();
+            } else {
+                expect(gemString, 'Expected gemString to exist').to.exist;
+                expect(gemString).to.equal('Tourmaline and Topaz and Tanzanite and Iolite and Emerald and Diamond');
+            }
+        });
+    });
+    describe(`STRETCH: Colorful gems contains gems and colors`, function () {
+        it(`STRETCH: Colorful gems contains gems and colors`, function () {
+            let { colorfulGems } = testItems;
+            expect(colorfulGems).to.be.an('array');
+            expect(colorfulGems).to.eql(['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']);
+        });
+    });
+});
+
+/**
+ * If running the tests in the browser, call mocha.run()
+ */
+if (typeof window === 'object') {
+    mocha.run();
+}
