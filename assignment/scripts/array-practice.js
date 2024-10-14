ed26f8cdc4b221784e20646fdff6035af48a9b6e
Zoe Copoulos <zoe.copoulos@gmail.com>
2024-10-14 13:20:26 -0500

    finished 3.1

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -1,21 +1,28 @@
 console.log('****** Array Practice *******');
 
+
 // Creating arrays
 console.log('--- 1. Practice creating arrays ---');
+
 
 // Example: Creating an array of animals
 let animalArray = ['fish', 'cat', 'bird', 'dog'];
 console.log('Animals are: ', animalArray);
 
+
 // 1. TODO: Make an array `favoriteFoods` with some favorite foods.
 //    Be sure to console.log your array of foods to the console with
 //    a message, similar to the example above.
 
+
 let favoriteFoods = ['baklava' 'spanakopita', 'triopita', 'lamb']
 console.log('Favorite foods are: ', favoriteFoods)
 
+
 // Array.length: A property that tells you how many items are in a given array.
 console.log('--- 2. Length of an array ---');
+
+
 
 
 // Example: How many animals are in the array?
@@ -23,27 +30,36 @@ let numberOfAnimals = animalArray.length // 👈 Using the .length property!
 console.log('Number of animals:', numberOfAnimals);
 
 
+
+
 // 2. TODO: Create a variable `numberOfFoods` and use the .length property
 //    to assign it the number value of how many items are inside `favoriteFoods`.
 //    Don't forget to console.log `numberOfFoods` to make sure your code worked!
 
+
 let numberOfFoods = favoriteFoods.length
 console.log('Number of favorite foods: ', numberOfFoods)
+
 
 // Accessing array items
 console.log('--- 3. Accessing items in an array ---');
 
+
 // Example: Log the first animal from the array using its array index
 let firstAnimal = animalArray[0];
 console.log('First animal is', firstAnimal);
+
 
 // 3.a. TODO: Create a variable `secondAnimal` and assign it the value
 //      of the second item from the `animalArray`.
 //      Be sure to console.log this new variable to make sure your code
 //      worked as expected.
 
+
 let secondAnimal = animalArray[1];
 console.groupCollapsed('Second animal is', secondAnimal);  //unsure if we're supposed to be doing the foods instead of animals, but I guess that doesn't matter for the purpose
+
+
 
 
 // 3.b. TODO: Create a variable `lastAnimal` and assign it the value of
@@ -52,22 +68,29 @@ console.groupCollapsed('Second animal is', secondAnimal);  //unsure if we're sup
 //      sure that your code does what you want. (Never trust your code until
 //      you have proof that it works!)
 
+
 let lastAnimal = animalArray[3];
 console.log('Last animal is', lastAnimal);
 console.log('Animals are: ', animalArray);
 
-// 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length 
+
+// 3.c. (STRETCH) TODO: Refactor 3.b to use the `animalArray` variable's .length
 //       property, rather than the exact (hardcoded) index number of the last item.
+
+
+
 
 
 
 // Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
 
+
 // Example: Add an animal to the end of an array using the Array.push method.
 let animalToPush = 'penguin';
 animalArray.push(animalToPush);
 console.log('Added an animal to end,', animalArray);
+
 
 // 4.a. TODO: Create a variable `dessert` and assign it a string value of
 //      a dessert that you love.
@@ -75,14 +98,19 @@ console.log('Added an animal to end,', animalArray);
 //      array. (How can you be 100% certain this worked? 🤔)
 
 
+
+
 // Example: Add to the beginning of an array using the Array.unshift method.
 let animalToUnshift = 'walrus';
 animalArray.unshift(animalToUnshift);
 console.log(`Added an animal to beginning: ${animalArray}`);
 
+
 // 4.b. TODO: Create a variable `entree` and assign it a string value of
 //      an entree that you enjoy.
 //      Add the `entree` to the beginning of your `favoriteFoods` array.
+
+
 
 
 // Example: Remove the last animal by using the Array.pop method.
@@ -90,8 +118,10 @@ let removedAnimal = animalArray.pop();
 console.log('Removed the last animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
+
 // NOTE: Use the `colors` array for 4.c and 4.d.
 let colors = ['orange', 'yellow', 'green', 'teal'];
+
 
 // 4.c. TODO: The last value inside the `colors` array was added by mistake.
 //      Remove it from the array and store it inside a new variable called
@@ -101,28 +131,41 @@ let colors = ['orange', 'yellow', 'green', 'teal'];
 //      contains the value "teal".)
 
 
+
+
 // Example: Remove the first animal using the Array.shift method.
 removedAnimal = animalArray.shift();
 console.log('Removed the first animal', removedAnimal);
 console.log('The animals are now', animalArray);
 
+
 // 4.d. TODO: Remove the first value from the `colors` array and store it
 //      inside a new variable called `firstColor`.
-//      To verify that your code worked, console.log `firstColor`!  
+//      To verify that your code worked, console.log `firstColor`! 
+
+
+
+
 
 
 
 
 // STRETCH GOALS:
 
+
 let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
+
 
 // 5.a (STRETCH) TODO: Replace the second gem with 'Tourmaline'.
 //      Remember to console.log the updated array.
 
 
+
+
 // 5.b (STRETCH) TODO: Sort your gems array in reverse alphabetical order.
 //     Please console.log the sorted array.
+
+
 
 
 // 5.c (STRETCH) TODO: Create a variable `gemString` and assign it a string value
@@ -134,7 +177,9 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 //     Don't forget to console.log `gemString`...
 
 
-// 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines 
+
+
+// 5.d (STRETCH) TODO: Make a new array `colorfulGems` that combines
 //     the gems array with the colors array.
 //     It should look something like:
 //       ['Tourmaline', 'Topaz', 'Tanzanite', 'Iolite', 'Emerald', 'Diamond', 'yellow', 'green']
@@ -143,23 +188,28 @@ let gems = ['Topaz', 'Citrine', 'Diamond', 'Emerald', 'Iolite', 'Tanzanite'];
 
 
 
+
+
+
+
 // DO NOT MODIFY
 // Used for automated testing
 try {
-  module.exports = {
-    favoriteFoods: typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
-    numberOfFoods: typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
-    secondAnimal: typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
-    lastAnimal: typeof lastAnimal !== 'undefined' ? lastAnimal : undefined,
-    dessert: typeof dessert !== 'undefined' ? dessert : undefined,
-    entree: typeof entree !== 'undefined' ? entree : undefined,
-    lastColor: typeof lastColor !== 'undefined' ? lastColor : undefined,
-    firstColor: typeof firstColor !== 'undefined' ? firstColor : undefined,
-    gems: typeof gems !== 'undefined' ? gems : undefined,
-    gemString: typeof gemString !== 'undefined' ? gemString : undefined,
-    colorfulGems: typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
-    colors: typeof colors !== 'undefined' ? colors : undefined,
-  };
+ module.exports = {
+   favoriteFoods: typeof favoriteFoods !== 'undefined' ? favoriteFoods : undefined,
+   numberOfFoods: typeof numberOfFoods !== 'undefined' ? numberOfFoods : undefined,
+   secondAnimal: typeof secondAnimal !== 'undefined' ? secondAnimal : undefined,
+   lastAnimal: typeof lastAnimal !== 'undefined' ? lastAnimal : undefined,
+   dessert: typeof dessert !== 'undefined' ? dessert : undefined,
+   entree: typeof entree !== 'undefined' ? entree : undefined,
+   lastColor: typeof lastColor !== 'undefined' ? lastColor : undefined,
+   firstColor: typeof firstColor !== 'undefined' ? firstColor : undefined,
+   gems: typeof gems !== 'undefined' ? gems : undefined,
+   gemString: typeof gemString !== 'undefined' ? gemString : undefined,
+   colorfulGems: typeof colorfulGems !== 'undefined' ? colorfulGems : undefined,
+   colors: typeof colors !== 'undefined' ? colors : undefined,
+ };
 } catch (e) {
-  // Do nothing
+ // Do nothing
 }
+
